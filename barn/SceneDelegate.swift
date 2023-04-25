009b305fcab9cdce3ffbe6d89bc26afe6f48d2d9
yucedagEmre <emrethedev@gmail.com>
2023-04-25 21:01:36 +0300

    homeViewController added

--- barn/SceneDelegate.swift
+++ barn/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
+        window?.rootViewController = HomeViewController()
         window?.makeKeyAndVisible()
     }
 
