07ac2abd1b450fadaa1dbd7da1befabbeec7063b
yucedagEmre <emrethedev@gmail.com>
2023-04-25 21:31:45 +0300

    homeViewCreated

---
+++ barn/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  barn
+//
+//  Created by Tugay Emre Yucedag on 25.04.2023.
+//
+
+import UIKit
+
+class HomeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
